d52200b8000f87f9658435bd43a9b868d2bba60a
Preston <prestonosoro56@gmail.com>
2022-11-23 02:04:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+    if(feet <= 400){
+      return "This one is on me!"
+    }
+    else if (feet>2000 && feet <=2500 ){
+      return 'I will gladly take your thirty bucks.';
+    }
+    else if (feet>=2501){
+      return 'No can do.';
+    }
+    }
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." :"No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip){
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+    return 'Thank you.';
+    break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'thanks for everything':
+      return 'Bye.';
+  }
 }
